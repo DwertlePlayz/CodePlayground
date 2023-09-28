0c78b61f24c65ae6ca61d9de3b439406955e168b
Dwertle <Dwertleplayz@gmail.com>
2023-09-27 22:19:48 -0400

    Added text repeat feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World" << std::endl;
+//init vars
+using namespace std;
+int reps;
+string phrase;
+
+int main() {
+
+    cout << "This program will take your input as a string and then output it the amount of times you request. Please input the text you want repeated." << endl;
+    getline(cin, phrase);
+    cout << "Now please enter the amount of times this should be repeated (number form)" << endl;
+    cin >> reps;
+    for(int i = 0; i <= reps; i++) {
+        cout << phrase << endl;
+    }
 }
